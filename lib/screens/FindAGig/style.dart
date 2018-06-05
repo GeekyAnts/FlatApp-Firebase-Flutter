fd0338b302ee70285ca5e0d5059011fa8a7dd107
Nkululeko Thangelane <nkululekoc1@gmail.com>
2018-06-05 20:20:58 +0200

    changes made on PC

---
+++ lib/screens/FindAGig/style.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+DecorationImage backroundfg =new DecorationImage(
+    image:  new ExactAssetImage("assets/Cluster/FindAGig/backroundfg.png"),
+    fit: BoxFit.fitWidth);
+
+DecorationImage Danish =new DecorationImage(
+    image:  new ExactAssetImage("assets/Cluster/FindAGig/Danish.png"),
+    fit: BoxFit.fitWidth);
+
+ExactAssetImage logo = new ExactAssetImage("assets/Cluster/Login/PLogo.png");
+
+TextStyle findGigHeader = new TextStyle(
+    fontSize: 40.0,
+    color: Colors.white,
+    fontWeight: FontWeight.bold
+
+
+
+);
